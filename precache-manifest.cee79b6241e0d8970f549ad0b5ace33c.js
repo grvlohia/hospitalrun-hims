8cab03a3ddcc9f7f4685af645a26d4521c445bad
Gaurav Lohia <gaurav.lohia.cse14@iitbhu.ac.in>
2021-08-26 17:56:43 +0530

    Updates

---
+++ precache-manifest.cee79b6241e0d8970f549ad0b5ace33c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9f909d0ef689937bbc144cd392b414",
+    "url": "/hospitalrun-hims/index.html"
+  },
+  {
+    "revision": "1f8836ec354d7067784a",
+    "url": "/hospitalrun-hims/static/css/2.4093b0d2.chunk.css"
+  },
+  {
+    "revision": "abee95b0c62b3a67de4d",
+    "url": "/hospitalrun-hims/static/css/main.9c66a029.chunk.css"
+  },
+  {
+    "revision": "1f8836ec354d7067784a",
+    "url": "/hospitalrun-hims/static/js/2.5f6a9015.chunk.js"
+  },
+  {
+    "revision": "7eb268268b9297386026d93d5465db88",
+    "url": "/hospitalrun-hims/static/js/2.5f6a9015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abee95b0c62b3a67de4d",
+    "url": "/hospitalrun-hims/static/js/main.f320b957.chunk.js"
+  },
+  {
+    "revision": "b7fe39418c4f31ef3e4e",
+    "url": "/hospitalrun-hims/static/js/runtime-main.527c23c8.js"
+  }
+]);
